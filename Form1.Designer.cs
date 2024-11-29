2e7d6915c3f7908a180e0e95943e6363e7dd5b1b
Karim Aouaouda <karimaouaouda.officiel@gmail.com>
2024-11-29 22:34:09 +0100

    hgjhg

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblProductName = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Location = new System.Drawing.Point(87, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(582, 100);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(430, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 108);
+            this.label1.TabIndex = 1;
+            this.label1.Text = " 0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 75);
+            this.button1.TabIndex = 2;
+            this.button1.Text = " ADD ITEM";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(581, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 108);
+            this.label2.TabIndex = 3;
+            this.label2.Text = " DA";
+            // 
+            // lblProductName
+            // 
+            this.lblProductName.AutoSize = true;
+            this.lblProductName.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductName.Location = new System.Drawing.Point(473, 339);
+            this.lblProductName.Name = "lblProductName";
+            this.lblProductName.Size = new System.Drawing.Size(296, 39);
+            this.lblProductName.TabIndex = 4;
+            this.lblProductName.Text = "Name OF Product";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(117, 339);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(249, 39);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Product Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblProductName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblProductName;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,84 @@ namespace Store
 {
     public partial class Form1 : Form
     {
+        struct Product
+        {
+            public string Name;
+            public int Prix;
+            public string Code;
+            public int _Contiti;
+            public Product(string name, int prix, string code,int Contiti)
+            {
+                Name = name;
+                Prix = prix;
+                Code = code;
+                _Contiti = Contiti;
+            }
+        }
+
+
+        Product[] products =new Product[3];
+
+        List <Product> Products;
+
+        int PrivRandom = 0;
+        void AddProductRandomly(Product[] products,int RandomProduct)
+        {
+            
+            if (RandomProduct== PrivRandom)
+            {
+                products[RandomProduct]._Contiti++;
+                dataTable.Clear();
+                foreach (Product product in products)
+                {
+                    dataTable.Rows.Add(product.Name, product.Prix, product.Code, product._Contiti);
+                }
+                dataGridView1.DataSource = dataTable;
+            }
+            PrivRandom = RandomProduct;
+            Price = Price + products[RandomProduct].Prix;
+            lblProductName.Text= products[RandomProduct].Name;  
+            label1.Text = Price.ToString();
+        }
+
         public Form1()
         {
+
             InitializeComponent();
+        }
+
+         public DataTable dataTable = new DataTable();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            products[0] = new Product("Laptop", 10, "P101",0);
+            products[1] = new Product("Smartphone", 20, "P102", 0);
+            products[2] = new Product("Headphones", 15, "P103", 0);
+
+            dataTable.Columns.Add("Name", typeof(string));
+            dataTable.Columns.Add("Prix", typeof(int));
+            dataTable.Columns.Add("Code", typeof(string));
+            dataTable.Columns.Add("Contiti", typeof(string));
+
+            // Fill the DataTable with data from the array
+            foreach (Product product in products)
+            {
+                dataTable.Rows.Add(product.Name, product.Prix, product.Code,product._Contiti);
+            }
+            //dataGridView1.Columns[0].HeaderText = "ID";
+            //dataGridView1.Columns[1].HeaderText = "Code";
+            //dataGridView1.Columns[2].HeaderText = "Name";
+            //dataGridView1.Columns[2].HeaderText = "Prix";
+            dataGridView1.DataSource = dataTable;
+        }
+
+        public int Price = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rn = new Random();
+            int RanProduct = rn.Next(0, products.Length);
+            AddProductRandomly(products, RanProduct);
+
         }
     }
 }
